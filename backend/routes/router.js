bad8b60e76c5764d02282135176ee8f5110fe002
falso <falsonombre04@gmail.com>
2025-01-24 10:37:35 -0500

    Segundo commit validadores agregados

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router();
 const {createAmigo,getAmigos,getAmigoById,getAmigoByIdAndUpdate,getAmigoAndDelete} = require('../controllers/agendaController')
+const {validarAgenda} = require('../validators/validarAgenda');
+const validar = require('../middleware/validar')
 
 router.get('/amigos',getAmigos);
 router.get('/amigos/amigo/:id',getAmigoById);
-router.post('/amigos/create',createAmigo);
-router.put('/amigos/amigo/:id',getAmigoByIdAndUpdate);
+router.post('/amigos/create',validarAgenda,validar,createAmigo);
+router.put('/amigos/amigo/:id',validarAgenda,validar,getAmigoByIdAndUpdate);
 router.delete('/amigos/amigo/:id',getAmigoAndDelete);
 
 module.exports = router;
--- backend/validators/validarAgenda.js
+++ backend/validators/validarAgenda.js
@@ -1,1 +1,20 @@
-const {body}
+const {body} = require('express-validator');
+
+const validarAgenda = [
+	body('nombre')
+	.notEmpty().withMessage("Campo nombre Obligatorio.")
+	.isString().withMessage("El valor debe ser un texto."),
+
+	body('numero')
+	.notEmpty().withMessage("Campo numero es Obligatario."),
+
+	body('edad')
+	.notEmpty().withMessage("Campo edad Obligatario.")
+	.isNumeric().withMessage("El campo debe ser un numero."),
+
+	body('correo')
+	.notEmpty().withMessage("Campo correo debe ser obligatorio.")
+	.isEmail().withMessage("Debe ser un correo valido.")
+]
+
+module.exports = {validarAgenda}
